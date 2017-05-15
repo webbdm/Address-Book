d0db6e390c5931621ef11e762a34f71ddaebef15
webbdm <gwebb762@gmail.com>
2017-05-14 20:27:43 -0500

    Connected Firebase

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,37 @@
-var app = angular.module("Address", []); 
+app.run((FIREBASE_CONFIG) => {
+    firebase.initializeApp(FIREBASE_CONFIG);
+});
 
-app.controller("MainCtrl", ($scope) => { 
-    $scope.thing = "Address";
+app.controller("MainCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+    $scope.addresses = [];
+
+    let getAddresses = () => {
+        let itemz = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/addresses.json`)
+                .then((fbItems) => {
+                    let itemCollection = fbItems.data;
+                    Object.keys(itemCollection).forEach((key) => {
+                        itemCollection[key].id = key;
+                        itemz.push(itemCollection[key]);
+                    });
+                    resolve(itemz);
+                }).catch((error) => {
+                    reject(error);
+                });
+        });
+
+    };
+
+    let getItems = () => {
+        getAddresses().then((itemz) => {
+            $scope.addresses = itemz;
+            console.log($scope.addresses);
+        }).catch((error) => {
+            console.log("get Error", error);
+        });
+    };
+
+    getItems();
 
 });
